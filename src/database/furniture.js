b1ad70a9ee3e4d1c6fae124622ac30e7a1196c7d
Somkene Ojukwu <somkeneoj@gmail.com>
2024-09-21 23:30:16 +0100

    model updated

--- src/database/furniture.js
+++ src/database/furniture.js
@@ -3,9 +3,9 @@ import { v4 as uuidv4 } from 'uuid';
 // This is the furniture  category 
 export const furnitureProducts = [
     { id: uuidv4(), name: "Modern Sofa", price: 500.00, imageSrc: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' },
-    { id: uuidv4(), name: "Wooden Coffee Table", price: 200.00, imageSrc: "https://images.unsplash.com/photo-1506898667547-42e22a46e125?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' },
-    { id: uuidv4(), name: "Leather Armcfurniture", price: 350.00, imageSrc: "https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' },
-    { id: uuidv4(), name: "Bedroom Wardrobe", price: 800.00, imageSrc: "https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' },
-    { id: uuidv4(), name: "Dining Table Set", price: 750.00, imageSrc: "https://images.unsplash.com/photo-1491926626787-62db157af940?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' },
-    { id: uuidv4(), name: "Bookshelf", price: 180.00, imageSrc: "https://images.unsplash.com/photo-1540574163026-643ea20ade25?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' }
+    { id: uuidv4(), name: "Single seater", price: 200.00, imageSrc: "https://images.unsplash.com/photo-1506898667547-42e22a46e125?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' },
+    { id: uuidv4(), name: "Grey single chair", price: 350.00, imageSrc: "https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' },
+    { id: uuidv4(), name: "The white mini sofa", price: 800.00, imageSrc: "https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' },
+    { id: uuidv4(), name: "Grey Sofa", price: 750.00, imageSrc: "https://images.unsplash.com/photo-1491926626787-62db157af940?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' },
+    { id: uuidv4(), name: "Triple Brown sofa", price: 180.00, imageSrc: "https://images.unsplash.com/photo-1540574163026-643ea20ade25?w=800&h=800&auto=format&fit=crop&q=60", category: 'furniture' }
 ];
