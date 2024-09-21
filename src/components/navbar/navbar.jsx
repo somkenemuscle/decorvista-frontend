1a9941b288c8e3960cac0e2057671a839e1cf543
Somkene Ojukwu <somkeneoj@gmail.com>
2024-09-21 04:53:15 +0100

    model updated

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -46,7 +46,7 @@ function Navbar() {
 
 
                             <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/about">About us</Link>
+                                <Link className="nav-link" aria-current="page" to="/about">About Us</Link>
                             </li>
                             {foundUsername ? (
                                 <li className="nav-item">
